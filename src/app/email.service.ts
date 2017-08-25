f37933aa52dfb59543c22d7afe88090462a0828a
Samuel Pedro <samuel.pedro@pmasolutions.com>
2017-08-25 11:58:45 +0000

    Generating Services Using Angular CLI

---
+++ src/app/email.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class EmailService {
+
+  constructor() { }
+
+}
+/**
+ * 
+ * we used 
+ *  ng g s email
+ * to create 2 files
+ *  - email.service.ts
+ *  - email.service.spec.ts
+ * 
+ */
